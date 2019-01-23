c6b9cd15efec52d82f2553e73fbe49005d9a4acb
ahmed4545 <34707771+ahmed4545@users.noreply.github.com>
2019-01-23 21:59:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,14 +5,14 @@ console.log("Welcome");
  
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`https://discord.gg/5tq4npH`)
+  return channel.send(`https://discord.gg/TBsGj28`)
 }).catch(console.error)
 
 })
  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`https://discord.gg/5tq4npH`) 
+  return channel.send(`https://discord.gg/TBsGj28`) 
 }).catch(console.error)
 })
 client.login(process.env.BOT_TOKEN);
